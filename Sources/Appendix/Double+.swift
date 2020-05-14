2883cf43b7ae9a2b8241634c2985f3343ef6848b
Nathan Borror <nathan@nathanborror.com>
2020-05-14 12:54:28 -0700

    better name

--- Sources/Appendix/Double+.swift
+++ Sources/Appendix/Double+.swift
@@ -2,8 +2,8 @@ import Foundation
 
 public extension Double {
 
-    // Returns a human readable double string (i.e. 1.2 or 1.23)
-    var roundUp: String {
+    /// Returns a human readable double string (i.e. 1.2 or 1.23)
+    var humanize: String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
         formatter.roundingMode = .halfUp
