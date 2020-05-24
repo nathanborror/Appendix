dce7f31d219a1058acb69c859bf19b853b06412e
Nathan Borror <nathan@nathanborror.com>
2020-05-24 10:47:45 -0700

    supporting macos

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     name: "Appendix",
     platforms: [
         .iOS(.v13),
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "Appendix", targets: ["Appendix"]),
